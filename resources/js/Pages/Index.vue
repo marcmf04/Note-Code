bf017f243ddcae236c5a9f90a2775ef278911cf0
joan <jpaneque@cendrassos.net>
2024-03-07 00:32:47 +0100

    Adios a dakar

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -5,10 +5,15 @@
 <template>
     <h1>Hola dakar que tal como estas</h1>
     <h1>Hola Powzito como estás bebe?</h1>
+    <h2>Adios dakarset</h2>
 </template>
 
 <style scoped>
     h1 {
         color: aquamarine;
+    }
+
+    h2 {
+        color: red;
     }
 </style>

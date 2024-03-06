dccfac93f975aa3c669dc215518810b162f4c64d
joan <jpaneque@cendrassos.net>
2024-03-07 00:21:07 +0100

    Saludar a dakar

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -3,9 +3,11 @@
 </script>
 
 <template>
-    <h1>first time here lol</h1>
+    <h1>Hola dakar que tal como estas</h1>
 </template>
 
 <style scoped>
-
+    h1 {
+        color: green;
+    }
 </style>

a7cdf8748a3f03f609b959f0929634ea9a304ad3
Marc Marqués Fernández <marcmf2004@gmail.com>
2024-03-22 05:00:44 +0100

    First part of header design done

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,10 +1,10 @@
 <script setup>
-
+    import Header from '@/Components/Index/Header.vue';
 </script>
 
 <template>
     <div class="index-container">
-
+        <Header />
     </div>
 </template>
 
